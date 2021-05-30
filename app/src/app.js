ca2b61ff504d0b897205335575b3d974803cfd12
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-05-30 16:31:21 +0300

    Test axios

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,21 @@
+import axios from 'axios';
 import './styles.scss';
 
 import '../widgets/lang/templates/lang-dropdown/lang-dropdown';
 
 import '../components/animation/animation';
 import '../components/drawing/drawing';
+
+const getSite = async () => {
+  try {
+    const siteUrl = "https://shafa.ua/women?search_text=Dior&sort=4";
+    const result = await axios.get(siteUrl);
+  
+    console.log('result', result);
+  } catch (error) {
+    console.log('error', error);
+  }
+  
+}
+
+getSite();
