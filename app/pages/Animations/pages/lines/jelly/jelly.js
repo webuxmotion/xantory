0c581194c59b4c3ee8e880d77a78649fe33128bd
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-11-07 22:17:38 +0200

    Jelly EmoMoe

--- app/pages/Animations/pages/lines/jelly/jelly.js
+++ app/pages/Animations/pages/lines/jelly/jelly.js
@@ -4,11 +4,13 @@ import Ball from './ball';
 const canvas = document.querySelector('#jelly');
 
 if (canvas) {
+  canvas.style.cursor = 'none';
   const mouse = new Mouse(canvas);
   const ctx = canvas.getContext('2d');
   const balls = [];
   const mouseBall = new Ball(0, 0, 50, 'green');
   const ballsCount = 12;
+  let image = null;
 
   for (let i = 0; i < ballsCount; i++) {
     balls.push(
@@ -20,6 +22,21 @@ if (canvas) {
   }
 
   render();
+  initSvg();
+
+  function initSvg() {
+    image = new Image();
+    image.src = "/img/emomoe-alt.svg";
+    image.onload = function() {
+      drawImage(0, 0);
+    }
+  }
+
+  function drawImage(x, y) {
+    if (image) {
+      ctx.drawImage(image, x - 50, y - 50);
+    }
+  }
 
   function render() {
     window.requestAnimationFrame(render);
@@ -31,8 +48,13 @@ if (canvas) {
     });
 
     mouseBall.setPosition(mouse.x, mouse.y);
-    mouseBall.draw(ctx);
+    //mouseBall.draw(ctx);
     connectDots(balls);
+
+    // var path = new Path2D('M60.5255 65.7696L66.1824 60.1127L53.4546 47.3849L66.1825 34.6569L60.5257 29L47.7977 41.728L35.0697 29L29.4128 34.6569L42.1408 47.3849L29.413 60.1127L35.0698 65.7696L47.7977 53.0417L60.5255 65.7696Z');
+    // ctx.stroke(path);
+
+    drawImage(mouse.x, mouse.y);
   }
 
   function connectDots(dots) {
